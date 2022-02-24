f9a90fa069f5a442980037db2b6eec49f4f10323
anonymous anonymous <nehal.garg@kellogg.northwestern.edu>
2022-02-24 05:15:44 +0000

    Display associated collections on show pages

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -42,8 +42,14 @@ class FurnituresController < ApplicationController
   # DELETE /furnitures/1
   def destroy
     @furniture.destroy
-    redirect_to furnitures_url, notice: 'Furniture was successfully destroyed.'
+    message = "Furniture was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to furnitures_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
