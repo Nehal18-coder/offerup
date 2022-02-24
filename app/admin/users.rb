66bb6c9ec386ed8325752a7ef68094bf12f5bf42
anonymous anonymous <nehal.garg@kellogg.northwestern.edu>
2022-02-24 05:15:37 +0000

    Whitelist columns in User for ActiveAdmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register User do
 
+ permit_params :name, :email, :password, :furniture_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
