21c142a03eda6961a1c0ad3abb0f100d74669d1a
anonymous anonymous <nehal.garg@kellogg.northwestern.edu>
2022-02-24 05:15:39 +0000

    Whitelist columns in Status for ActiveAdmin

--- app/admin/statuses.rb
+++ app/admin/statuses.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Status do
 
+ permit_params :furniture_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
