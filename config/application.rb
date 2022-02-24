748579e249a5470a7b64a460d8b999f4668c2229
anonymous anonymous <nehal.garg@kellogg.northwestern.edu>
2022-02-24 05:16:55 +0000

    Disable protect_from_forgery setting

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,8 @@ module Offerup
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.action_controller.default_protect_from_forgery = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
